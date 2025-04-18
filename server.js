3cb41f7e0e8626c1b166ac8b4447a04cb1118422
jootaef <jaydanv1008@gmail.com>
2025-04-17 21:01:49 -0500

    q

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: process.env.CALLBACK_URL,
+      callbackURL: process.env.GITHUB_CALLBACK_URL, // ✅ corregido aquí
     },
     function (accessToken, refreshToken, profile, done) {
       return done(null, profile);
